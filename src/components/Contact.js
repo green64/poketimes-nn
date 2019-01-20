c9d440e67cb4508dd1c9396ae3d311221f6c47f4
Samantha McCall <samanthaadams.biz@gmail.com>
2019-01-20 15:30:07 -0600

    new site using react router and higher level components

---
+++ src/components/Contact.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+//adding props here adds router info automatically 
+const Contact = (props) => {
+  // programatic redirect by using props
+  setTimeout(() => {
+    props.history.push('/about')
+  }, 2000);
+  return(
+    <div className="container">
+      <h4 className="center">Contact</h4>
+      <p>A accumsan est consectetur sed. Praesent gravida at lacus a aliquam. Quisque vitae sagittis nisi. Vivamus porttitor orci sit amet ex placerat, vitae rhoncus velit volutpat. Aliquam sit amet purus et diam bibendum lobortis vehicula eu quam. Donec eget venenatis libero. Nam at sapien suscipit, mattis eros ac, viverra nibh. Phasellus consequat iaculis arcu sit amet pulvinar. Etiam ullamcorper vestibulum est sit amet fringilla. Curabitur vehicula neque ac massa scelerisque, ac tincidunt est gravida. Maecenas sagittis erat rutrum massa euismod suscipit. Cras at quam non velit venenatis pellentesque ut vel nibh. Nullam laoreet dignissim tellus.</p>
+    </div>
+  )
+}
+
+export default Contact
---
+++ src/components/Navbar.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import { Link, NavLink, withRouter } from 'react-router-dom'
+
+const Navbar = (props) => {
+  setTimeout(() => {
+    props.history.push('/about')
+  }, 2000);
+  return (
+    <nav className="nav-wrapper red darken-3">
+      <div className="container">
+        <a className="brand-logo">Poke Times</a>
+        <ul className="right">
+          {/* instead of href we use Link to so react isn't reloading index, about, etc */}
+          <li><Link to ="/">Home</Link></li>
+          {/* NavLink adds class to href which could be useful -- Link doesn't add it  */}
+          <li><NavLink to ="/about">About</NavLink></li>
+          <li><NavLink to ="/contact">Contact</NavLink></li>
+        </ul>
+      </div>
+    </nav>
+  )
+}
+
+export default withRouter(Navbar)
